c86def46a09233ecb36840631758143ed62bd01b
Like4Schnitzel <rw21062006@gmail.com>
2023-09-28 13:24:22 +0200

    write headers

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+
+</script>
+
+<svelte:head>
+    <title>Name Browser for Nerds</title>
+    <meta content="Name Browser for Nerds" property="og:title" />
+    <meta content="A Name Browser, for Nerds" property="og:description" />
+    <meta content="https://like4schnitzel.github.io/NBFN/" property="og:url" />
+    <meta content="https://like4schnitzel.github.io/NBFN/imgs/icon.png" property="og:image" />
+    <meta content="#F9AEC4" data-react-helmet="true" name="theme-color" />
+    <link rel="icon" href="icon.png"/>
+    <meta charset="UTF-8"/>
+    <html lang="en"/>
+</svelte:head>
+
+<style>
+    
+</style>
