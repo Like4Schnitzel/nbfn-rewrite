657731cc9d6c8d659ce2294e399cf9326f2975c9
Like4Schnitzel <rw21062006@gmail.com>
2023-09-29 11:22:11 +0200

    fix head

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,8 @@
 
 </script>
 
-<svelte:head>
+
+<head>
     <title>Name Browser for Nerds</title>
     <meta content="Name Browser for Nerds" property="og:title" />
     <meta content="A Name Browser, for Nerds" property="og:description" />
@@ -10,13 +11,11 @@
     <meta content="https://like4schnitzel.github.io/NBFN/imgs/icon.png" property="og:image" />
     <meta content="#F9AEC4" data-react-helmet="true" name="theme-color" />
     <link rel="icon" href="icon.png"/>
-    <meta charset="UTF-8"/>
-    <html lang="en"/>
-</svelte:head>
+</head>
 
-<div class="main">
+<body class="main">
     <h1>NBFN</h1>
-</div>
+</body>
 
 <style>
     .main {
